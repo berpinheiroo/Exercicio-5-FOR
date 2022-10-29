f25964a53168d1963ed93bc720c0f0a0d3e07453
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-29 20:44:00 -0300

    Mudando de repositorio

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Desenvolva um algoritmo que usando a estrutura FOR mostre no console uma
+contagem de 0 a 50, porém, essa contagem tem que ser feita de 5 em 5.
+Ex.: 0 5 10 15 20 25 … 45 50 Fim! */
+
+var valor = 0
+
+for (i = 50; i >= valor; i--) {
+   console.log(i)
+   i = i - 4
+    
+}
